270485b0cb41f51cdbe5c062bf1b9b6958154c9f
Shakil Khan <shakilkhan81198@gmail.com>
2024-08-24 17:48:10 +0600

    login page added

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,19 +1,15 @@
-// eslint-disable-next-line react/prop-types
+/* eslint-disable react/prop-types */
 const ServiceCard = ({ service }) => {
-  const { img, title, price  } = service;
+  const { img, title, price } = service;
 
   return (
     <div className="card bg-base-100 w-96 shadow-xl">
       <figure className="px-10 pt-10">
-        <img
-          src={img}
-          alt="Shoes"
-          className="rounded-xl"
-        />
+        <img src={img} alt="Shoes" className="rounded-xl" />
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{title}</h2>
-        <p>{price}</p>
+        <p className="text-orange-500">Price: ${price}</p>
         <div className="card-actions">
           <button className="btn btn-primary">Buy Now</button>
         </div>
--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -21,7 +21,7 @@ const Services = () => {
           believable.
         </p>
       </div>
-      <div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {services.map((service) => (
           <ServiceCard key={service._id} service={service}></ServiceCard>
         ))}
